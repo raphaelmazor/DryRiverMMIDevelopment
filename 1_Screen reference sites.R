3f93afb1944c962de61b8a1768908a946b955003
raphaelmazor <raphaelm@sccwrp.org>
2023-07-25 09:24:37 -0700

    Started script on screening ref sites

---
+++ 1_Screen reference sites.R
@@ -0,0 +1,16 @@
+#########################
+###Reference screening###
+#########################
+
+library(tidyverse)
+library(ggplot2)
+
+######LOAD DATA######
+
+#Load GIS data
+
+#Load habitat data
+
+#Load bio data, for assessing sensitivity
+
+#####
