396f215a637a2f410a60142a099422f8aba475ed
raphaelmazor <raphaelm@sccwrp.org>
2023-07-25 10:31:36 -0700

    Added arthropod metrics.

--- 1_Screen reference sites.R
+++ 1_Screen reference sites.R
@@ -12,5 +12,24 @@ library(ggplot2)
 #Load habitat data
 
 #Load bio data, for assessing sensitivity
+arthro_df<-read_csv("Data/BioData/Arthros/arthropod_metircs_07252023.csv")
 
-#####
+#####ASSESS BIO RESPONSES TO STRESS#####
+
+#Calculate Spearman's Rho between all bio metrics and all hab/gis metrics [stressors only]
+#Create scatterplots of strong relationships
+
+##Sepeicifically, look at phab stress measures
+#How correlated are they to eachother?
+#Look at hab stress metrics and identify appropriate cutoffs through visual inspections of relationships
+#Can you pick the "best" one?
+#How concordant are they based on cutoffs?
+#Is one more sensitive than the others?
+
+####SCREEN
+
+#Start with Ode et al. 2016 screens
+#Replace w1_hall with best phab metric(s)
+#Adjust gis screens if necessary based on observed relationships with biology
+
+
