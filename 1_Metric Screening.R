183b5ff382d62e82f4c923807a8cda6f5163e82c
raphaelmazor <raphaelm@sccwrp.org>
2023-07-26 16:40:38 -0700

    added screening script

---
+++ 1_Metric Screening.R
@@ -0,0 +1,143 @@
+####Assess metric response to stress
+
+library(tidyverse)
+library(ggplot2)
+
+#Load GIS data
+
+#Load habitat data
+phab_df<-read_csv("Data/NonBioData/Habitat/PHAB_Metrics_07252023.csv") %>%
+  mutate(ReachStress = case_when(HumanActivity_Ext<12 &
+                                   HumanActivity_Int<12 & 
+                                   # HumanActivity_Prox<8 &
+                                   HumanActivity_Prox_SWAMP<6 ~0,
+                                 HumanActivity_Ext>=20~2,
+                                 HumanActivity_Int>=20~2,
+                                 # HumanActivity_Prox>=20~2,
+                                 HumanActivity_Prox_SWAMP>=10~2,
+                                 T~1 ))
+
+ref_sites<-phab_df %>%
+  group_by(StationCode) %>%
+  summarise(ReachStressMax = max(ReachStress, na.rm=T)) %>%
+  ungroup() %>%
+  mutate(StressLevel = case_when(ReachStressMax==0~"Low",
+                                 ReachStressMax==1~"Med",
+                                 ReachStressMax==2~"High",
+                                 T~"Other"))
+ref_sites %>% group_by(StressLevel) %>% tally()
+#Load bio data, for assessing sensitivity
+bug_mets_lu<-read_csv("Data/BioData/Arthros/bug_metric_lu_07252023.csv") %>%
+  mutate(Metric_short=str_sub(Metric, 3, nchar(Metric)))
+
+arthro_df<-read_csv("Data/BioData/Arthros/arthropod_metircs_07252023.csv")
+arth_df_long<-arthro_df %>%
+  select(StationCode, SampleDate, CollectionMethodCode, all_of(bug_mets_lu$Metric_short)) %>%
+  pivot_longer(cols=all_of(bug_mets_lu$Metric_short), 
+               names_to="Metric_short", 
+               values_to="MetricValue", 
+               values_drop_na = T) %>%
+  mutate(Metric = case_when(CollectionMethodCode=="TerInvt_T_DS"~paste0("T_",Metric_short),
+                            CollectionMethodCode=="TerInvt_V_DS"~paste0("V_",Metric_short),
+                            T~Metric_short)) %>%
+  #Get rid of these interim calc metrics
+  filter(!str_detect(Metric,"KRich")) %>%
+  filter(!str_detect(Metric,"KAbund")) %>%
+  inner_join(bug_mets_lu %>% select(Metric, Method, MetricForm, MetricGroup=Group))
+
+bryo_df<-read_csv("Data/BioData/Bryos/bryo_metrics_07262023_v2.csv")
+# bryo_df<-read_csv("Data/BioData/Bryos/Final_Bryophyte_Output.csv") 
+skimr::skim(bryo_df)
+
+bryo_df_long<-bryo_df %>%
+  select(-SampleID, -CollectionMethodCode, -Replicate) %>%
+  pivot_longer(cols=c(-StationCode, -SampleDate),
+               names_to = "Metric_short", values_to="MetricValue", values_drop_na = T) %>%
+  filter(MetricValue > -Inf & MetricValue<Inf) %>%
+  mutate(CollectionMethodCode="Bryophyte",
+         Method="Bryo",
+         MetricForm="Bryo", MetricGroup="Bryo", #Until I get lookup
+         Metric= paste0("B_",Metric_short))
+
+
+
+junk<-lm(MetricValue~StressLevel, 
+         data=arth_df_long %>% na.omit() %>%
+           filter(MetricValue>-Inf) %>%
+           left_join(ref_sites %>% select(StationCode, StressLevel)))
+junk<-t.test(
+  MetricValue~StressLevel, 
+  data=
+    arth_df_long %>% 
+    filter(MetricValue>-Inf) %>%
+    inner_join(
+      ref_sites %>% 
+        select(StationCode, StressLevel) ) %>%
+    filter(StressLevel %in% c("Low","High"))
+)
+junk %>% broom::glance()
+
+junk2<-junk %>% broom::glance()
+junk2$f
+
+metric_summary<-arth_df_long %>%
+  bind_rows(bryo_df_long) %>%
+  filter(MetricValue>-Inf) %>%
+  filter(!str_detect(Metric_short, "_KRich")) %>%
+  filter(!str_detect(Metric_short, "_KAbund")) %>%
+  left_join(ref_sites %>% select(StationCode, StressLevel)) %>%
+  group_by(Metric, Metric_short, MetricGroup, MetricForm) %>%
+  summarise(
+    unique_values = unique(MetricValue) %>% length(),
+    F_stat = lm(MetricValue~StressLevel) %>%
+      broom::glance() %>%
+      select(statistic) %>% 
+      unlist(),
+    
+    # t_stat = t.test(MetricValue~StressLevel, 
+    #                 data= .%>% filter(StressLevel!="Med")) %>%
+    #   broom::glance() %>%
+    #   select(statistic) %>% 
+    #   unlist()
+  ) %>%
+  ungroup() 
+
+metric_summary$t_stat <-sapply(1:nrow(metric_summary), function(i){
+  vals.i=metric_summary$unique_values[i]
+  if(vals.i==1)
+    0
+  else
+  {
+    met.i = metric_summary$Metric[i]
+    xdf = arth_df_long %>%
+      bind_rows(bryo_df_long) %>%
+      filter(MetricValue>-Inf) %>%
+      filter(Metric==met.i) %>%
+      left_join(ref_sites %>% select(StationCode, StressLevel)) %>%
+      filter(StressLevel %in% c("Low","High"))
+    t_xdf = t.test(MetricValue~StressLevel, data = xdf)
+    t_xdf$statistic
+  }
+})
+
+ggplot(metric_summary %>%
+         filter(unique_values>1), aes(x=F_stat))+
+  geom_histogram()+
+  facet_wrap(~MetricForm)+
+  geom_vline(xintercept=2, linetype="dashed", color="red")
+
+
+ggplot(metric_summary %>%
+         filter(unique_values>1) %>%
+         pivot_longer(cols=c(F_stat, t_stat))
+       , aes(x=abs(value)))+
+  geom_density(aes(fill=name), alpha=.5)+
+  facet_wrap(~MetricForm)+
+  geom_vline(xintercept=2, linetype="dashed", color="red")
+
+
+metric_summary %>%
+  filter(F_stat > 10)
+
+
+
--- 1_Screen reference sites.R
+++ 1_Screen reference sites.R
@@ -16,61 +16,63 @@ phab_df<-read_csv("Data/NonBioData/Habitat/PHAB_Metrics_07252023.csv")
 arthro_df<-read_csv("Data/BioData/Arthros/arthropod_metircs_07252023.csv")
 
 
+bug_mets_lu<-read_csv("Data/BioData/Arthros/bug_metric_lu_07252023.csv") %>%
+  mutate(Metric_short=str_sub(Metric, 3, nchar(Metric)))
+# bug_mets<-bug_mets_lu$Metric_short
+  
 
-bug_mets<-c("Arth_Rich", "Arth_Abund", 
-            "Insect_Rich", "Insect_RelRich","Insect_Abund", "Insect_RelAbund", 
-            "Noninsect_Rich", "Noninsect_RelRich", "Noninsect_Abund", "Noninsect_RelAbund",
-            "Coleo_Rich", "Coleo_RelRich", "Coleo_Abund", "Coleo_RelAbund", 
-            "Araneae_Rich", "Araneae_RelRich", "Araneae_Abund", "Araneae_RelAbund", 
-            "Araneae_Nat_Rich", "Araneae_Nat_RelRich", "Araneae_Nat_Abund", "Araneae_Nat_RelAbund",
-            "Formicidae_Rich", "Formicidae_RelRich", "Formicidae_Abund", "Formicidae_RelAbund",
-            "Formicidae_Nat_Rich", "Formicidae_Nat_RelRich", "Formicidae_Nat_Abund", "Formicidae_Nat_RelAbund", 
-            "CAF_Rich", "CAF_RelRich", "CAF_Abund", "CAF_RelAbund", 
-            "CAF_Nat_Rich", "CAF_Nat_RelRich", "CAF_Nat_Abund", "CAF_Nat_RelAbund", 
-            "Nonnat_KRich", "Nonnat_Rich", "Nonnat_RelRich", "Nonnat_KAbund", "Nonnat_Abund", "Nonnat_RelAbund", 
-            "NonnatSynanth_KRich", "NonnatSynanth_Rich", "NonnatSynanth_RelRich", 
-            "NonnatSynanth_KAbund", "NonnatSynanth_Abund", "NonnatSynanth_RelAbund", 
-            "LinEpi_Rich", "LinEpi_RelRich", "LinEpi_Abund", "LinEpi_RelAbund", 
-            "Aquatic_KRich", "Aquatic_Rich", "Aquatic_RelRich", "Aquatic_KAbund", "Aquatic_Abund", "Aquatic_RelAbund", 
-            "StratumFinal_KRich", "StratumFinal_KAbund",
-            "Ground_Rich", "Ground_RelRich", "Ground_Abund", "Ground_RelAbund", 
-            "Herbaceous_Rich", "Herbaceous_RelRich", "Herbaceous_Abund", "Herbaceous_RelAbund", 
-            "Arboreal_Rich", "Arboreal_RelRich", "Arboreal_Abund", "Arboreal_RelAbund", 
-            "FFG_KRich", "FFG_KAbund", 
-            "Detritivore_Rich", "Detritivore_RelRich", "Detritivore_Abund","Detritivore_RelAbund", 
-            "Fungivore_Rich", "Fungivore_RelRich", "Fungivore_Abund", "Fungivore_RelAbund",
-            "DetrFung_Rich", "DetrFung_RelRich","DetrFung_Abund", "DetrFung_RelAbund", 
-            "Predator_Rich", "Predator_RelRich", "Predator_Abund", "Predator_RelAbund", "FFG_d_KRich", "FFG_d_KAbund", "PredatorGround_Rich", "PredatorGround_RelRich", "PredatorGround_Abund","PredatorGround_RelAbund", 
-            "BodySize_KRich", "BodySize_KAbund", "BodySizeSmall_Rich", "BodySizeSmall_RelRich", "BodySizeSmall_Abund", "BodySizeSmall_RelAbund", "BodySizeMedium_Rich", "BodySizeMedium_RelRich", "BodySizeMedium_Abund", "BodySizeMedium_RelAbund", "BodySizeLarge_Rich", "BodySizeLarge_RelRich", "BodySizeLarge_Abund", "BodySizeLarge_RelAbund", "BodySizeLargest", "BodySizeAverage", 
-            "Dispersal_KRich", "Dispersal_KAbund", "DisperserPoor_Rich", "DisperserPoor_RelRich", "DisperserPoor_Abund", "DisperserPoor_RelAbund", "DisperserGood_Rich", "DisperserGood_RelRich", "DisperserGood_Abund", "DisperserGood_RelAbund", 
-            "TempTol_KRich", "TempTol_KAbund", "TempTolHigh_Rich", "TempTolHigh_RelRich", "TempTolHigh_Abund", "TempTolHigh_RelAbund", "TempTolAverage", "TempTolMax")
+# bug_mets<-c("Arth_Rich", "Arth_Abund", 
+#             "Insect_Rich", "Insect_RelRich","Insect_Abund", "Insect_RelAbund", 
+#             "Noninsect_Rich", "Noninsect_RelRich", "Noninsect_Abund", "Noninsect_RelAbund",
+#             "Coleo_Rich", "Coleo_RelRich", "Coleo_Abund", "Coleo_RelAbund", 
+#             "Araneae_Rich", "Araneae_RelRich", "Araneae_Abund", "Araneae_RelAbund", 
+#             "Araneae_Nat_Rich", "Araneae_Nat_RelRich", "Araneae_Nat_Abund", "Araneae_Nat_RelAbund",
+#             "Formicidae_Rich", "Formicidae_RelRich", "Formicidae_Abund", "Formicidae_RelAbund",
+#             "Formicidae_Nat_Rich", "Formicidae_Nat_RelRich", "Formicidae_Nat_Abund", "Formicidae_Nat_RelAbund", 
+#             "CAF_Rich", "CAF_RelRich", "CAF_Abund", "CAF_RelAbund", 
+#             "CAF_Nat_Rich", "CAF_Nat_RelRich", "CAF_Nat_Abund", "CAF_Nat_RelAbund", 
+#             "Nonnat_KRich", "Nonnat_Rich", "Nonnat_RelRich", "Nonnat_KAbund", "Nonnat_Abund", "Nonnat_RelAbund", 
+#             "NonnatSynanth_KRich", "NonnatSynanth_Rich", "NonnatSynanth_RelRich", 
+#             "NonnatSynanth_KAbund", "NonnatSynanth_Abund", "NonnatSynanth_RelAbund", 
+#             "LinEpi_Rich", "LinEpi_RelRich", "LinEpi_Abund", "LinEpi_RelAbund", 
+#             "Aquatic_KRich", "Aquatic_Rich", "Aquatic_RelRich", "Aquatic_KAbund", "Aquatic_Abund", "Aquatic_RelAbund", 
+#             "StratumFinal_KRich", "StratumFinal_KAbund",
+#             "Ground_Rich", "Ground_RelRich", "Ground_Abund", "Ground_RelAbund", 
+#             "Herbaceous_Rich", "Herbaceous_RelRich", "Herbaceous_Abund", "Herbaceous_RelAbund", 
+#             "Arboreal_Rich", "Arboreal_RelRich", "Arboreal_Abund", "Arboreal_RelAbund", 
+#             "FFG_KRich", "FFG_KAbund", 
+#             "Detritivore_Rich", "Detritivore_RelRich", "Detritivore_Abund","Detritivore_RelAbund", 
+#             "Fungivore_Rich", "Fungivore_RelRich", "Fungivore_Abund", "Fungivore_RelAbund",
+#             "DetrFung_Rich", "DetrFung_RelRich","DetrFung_Abund", "DetrFung_RelAbund", 
+#             "Predator_Rich", "Predator_RelRich", "Predator_Abund", "Predator_RelAbund", "FFG_d_KRich", "FFG_d_KAbund", "PredatorGround_Rich", "PredatorGround_RelRich", "PredatorGround_Abund","PredatorGround_RelAbund", 
+#             "BodySize_KRich", "BodySize_KAbund", "BodySizeSmall_Rich", "BodySizeSmall_RelRich", "BodySizeSmall_Abund", "BodySizeSmall_RelAbund", "BodySizeMedium_Rich", "BodySizeMedium_RelRich", "BodySizeMedium_Abund", "BodySizeMedium_RelAbund", "BodySizeLarge_Rich", "BodySizeLarge_RelRich", "BodySizeLarge_Abund", "BodySizeLarge_RelAbund", "BodySizeLargest", "BodySizeAverage", 
+#             "Dispersal_KRich", "Dispersal_KAbund", "DisperserPoor_Rich", "DisperserPoor_RelRich", "DisperserPoor_Abund", "DisperserPoor_RelAbund", "DisperserGood_Rich", "DisperserGood_RelRich", "DisperserGood_Abund", "DisperserGood_RelAbund", 
+#             "TempTol_KRich", "TempTol_KAbund", "TempTolHigh_Rich", "TempTolHigh_RelRich", "TempTolHigh_Abund", "TempTolHigh_RelAbund", "TempTolAverage", "TempTolMax")
 
 setdiff(bug_mets, names(arthro_df))
 setdiff(names(arthro_df), bug_mets)
 
+
+#Convert arth data to long format and correct the metric name
+
 arth_df_long<-arthro_df %>%
-  select(StationCode, SampleDate, CollectionMethodCode, all_of(bug_mets)) %>%
-  pivot_longer(cols=all_of(bug_mets), names_to="BugMet", values_to="BugMet_value", values_drop_na = T) %>%
-  mutate(BugMet2 = case_when(CollectionMethodCode=="TerInvt_T_DS"~paste0("T_",BugMet),
-                             CollectionMethodCode=="TerInvt_V_DS"~paste0("V_",BugMet),
-                             T~BugMet),
-         Method = case_when(CollectionMethodCode=="TerInvt_T_DS"~("Trap"),
-                            CollectionMethodCode=="TerInvt_V_DS"~("Veg"),
-                            T~"Other"),
-         MetricForm = case_when(str_detect(BugMet,"_Rich")~"Rich",
-                                str_detect(BugMet,"_RelRich")~"RelRich",
-                                str_detect(BugMet,"_Abund")~"Abund",
-                                str_detect(BugMet,"_RelAbund")~"RelAbund",
-                                T~"Other")) %>% #Other includes largest, average, and max
-  clipr::write_clip()
+  select(StationCode, SampleDate, CollectionMethodCode, all_of(bug_mets_lu$Metric_short)) %>%
+  pivot_longer(cols=all_of(bug_mets_lu$Metric_short), 
+               names_to="Metric_short", 
+               values_to="BugMet_value", 
+               values_drop_na = T) %>%
+  mutate(Metric = case_when(CollectionMethodCode=="TerInvt_T_DS"~paste0("T_",Metric_short),
+                             CollectionMethodCode=="TerInvt_V_DS"~paste0("V_",Metric_short),
+                             T~Metric_short)) %>%
   #Get rid of these interim calc metrics
-  filter(!str_detect(BugMet,"KRich")) %>%
-  filter(!str_detect(BugMet,"KAbund"))
+  filter(!str_detect(Metric,"KRich")) %>%
+  filter(!str_detect(Metric,"KAbund")) %>%
+  inner_join(bug_mets_lu %>% select(Metric, Method, MetricForm, MetricGroup=Group))
 
 arth_df_long %>% clipr::write_clip()
 
 # arth_df_long %>% filter(MetricForm=="Other") %>% select(BugMet2) %>% unique()
-# arth_df_long %>%   group_by(MetricForm) %>% tally()
+arth_df_long %>%   group_by(MetricForm) %>% tally()
 
 #####ASSESS BIO RESPONSES TO STRESS#####
 
@@ -84,29 +86,124 @@ arth_df_long %>% clipr::write_clip()
 #How concordant are they based on cutoffs?
 #Is one more sensitive than the others?
 
+#Identify habitat stress metrics
 hab_stress_mets<-c("HumanActivity_Ext",	"HumanActivity_Int",	"HumanActivity_Prox",	"HumanActivity_Prox_SWAMP")
 
-library(corrplot)
-stress_df<-phab_df %>%
+#Create a long-format dataframe with just these stress metrics
+phabstress_df<-phab_df %>%
   mutate(SampleDate=lubridate::mdy(SampleDate)) %>%
   select(StationCode, SampleDate, all_of(hab_stress_mets)) %>%
   #just work with the stress measures
-  pivot_longer(cols=all_of(hab_stress_mets), names_to="PHABMet", values_to = "PHABMet_value", values_drop_na = T)
+  pivot_longer(cols=all_of(hab_stress_mets), names_to="PHABMet", values_to = "PHABMet_value", values_drop_na = T) %>%
+  group_by(PHABMet) %>%
+  mutate(MetMax=max(PHABMet_value)) %>%
+  ungroup() %>%
+  mutate(PHABMet_value_rescaled=PHABMet_value/MetMax)
+# skimr::skim(phabstress_df)
+
+#Join with the long arthropod data to create scatterplots
+arth_phabstress<-inner_join(arth_df_long, phabstress_df, relationship = "many-to-many")
+
+phabstress_v_bugmets_richness<-ggplot(data=arth_phabstress %>%
+         filter(MetricForm=="Rich") ,
+       aes(x=PHABMet_value_rescaled, y=BugMet_value))+
+  geom_smooth(aes(color=PHABMet),  se=F)+
+  facet_grid(MetricGroup~Method, scales="free")+
+  theme_bw()+
+  theme(strip.text.y = element_text(angle=0))+
+  ggtitle("Richness metrics")
+ggsave(phabstress_v_bugmets_richness, filename="Figures/phabstress_v_bugmets_richness.png", height=8, width=8)
+
+phabstress_v_bugmets_relrichness<-ggplot(data=arth_phabstress %>%
+                                        filter(MetricForm=="RelRich") ,
+                                      aes(x=PHABMet_value_rescaled, y=BugMet_value))+
+  geom_smooth(aes(color=PHABMet),  se=F)+
+  facet_grid(MetricGroup~Method, scales="free")+
+  theme_bw()+
+  theme(strip.text.y = element_text(angle=0))+
+  ggtitle("Relative richness metrics")
+ggsave(phabstress_v_bugmets_relrichness, filename="Figures/phabstress_v_bugmets_relrichness.png", height=8, width=8)
+
+phabstress_v_bugmets_abund<-ggplot(data=arth_phabstress %>%
+                                           filter(MetricForm=="Abund") ,
+                                         aes(x=PHABMet_value_rescaled, y=BugMet_value))+
+  geom_smooth(aes(color=PHABMet),  se=F)+
+  facet_grid(MetricGroup~Method, scales="free")+
+  theme_bw()+
+  theme(strip.text.y = element_text(angle=0))+
+  ggtitle("Abundance metrics")
+ggsave(phabstress_v_bugmets_abund, filename="Figures/phabstress_v_bugmets_abund.png", height=8, width=8)
+
+phabstress_v_bugmets_relabund<-ggplot(data=arth_phabstress %>%
+                                     filter(MetricForm=="RelAbund") ,
+                                   aes(x=PHABMet_value_rescaled, y=BugMet_value))+
+  geom_smooth(aes(color=PHABMet),  se=F)+
+  facet_grid(MetricGroup~Method, scales="free")+
+  theme_bw()+
+  theme(strip.text.y = element_text(angle=0))+
+  ggtitle("Relative abundance metrics")
+ggsave(phabstress_v_bugmets_relabund, filename="Figures/phabstress_v_bugmets_relabund.png", height=8, width=8)
+
+phabstress_v_bugmets_other<-ggplot(data=arth_phabstress %>%
+                                        filter(MetricForm=="Other") ,
+                                      aes(x=PHABMet_value_rescaled, y=BugMet_value))+
+  geom_smooth(aes(color=PHABMet),  se=F)+
+  facet_grid(MetricGroup~Method, scales="free")+
+  theme_bw()+
+  theme(strip.text.y = element_text(angle=0))+
+  ggtitle("Other metrics")
+ggsave(phabstress_v_bugmets_other, filename="Figures/phabstress_v_bugmets_other.png", height=8, width=8)
+
+phabstress_df %>%
+  select(PHABMet, MetMax)  %>%
+  unique()
+
+
+
+
+# Propose cutoffs that divide the data set into ~thirds:
+# cutoffs_df<-crossing(
+#   Ext = 1:24,
+#   Int=1:24,
+#   Prox=seq(from=1, to=10, by=.5)
+# )
+# 
+# cutoffs_df$Low_n <- sapply(1:nrow(cutoffs_df), function(i){
+#   xdf = phab_df %>%
+#     filter(StationCode %in% arth_df_long$StationCode) %>%
+#     mutate(Stress=case_when(HumanActivity_Ext< cutoffs_df$Ext[i] &
+#                               HumanActivity_Int< cutoffs_df$Int[i] & 
+#                               # HumanActivity_Prox<8 &
+#                               HumanActivity_Prox_SWAMP<cutoffs_df$Prox[i] ~"Low",
+#                             T~"Not Low"))
+#   nrow(xdf %>%filter(Stress=="Low"))
+# })
+# 
+# cutoffs_df %>% filter(Low_n>=12)
+
+phab_df<-phab_df %>%
+  mutate(ReachStress = case_when(HumanActivity_Ext<12 &
+                                   HumanActivity_Int<12 & 
+                                   # HumanActivity_Prox<8 &
+                                   HumanActivity_Prox_SWAMP<6 ~"Low",
+                                 HumanActivity_Ext>=20~"High",
+                                 HumanActivity_Int>=20~"High",
+                                 # HumanActivity_Prox>=20~"High",
+                                 HumanActivity_Prox_SWAMP>=10~"High",
+                                 T~"Med" )) %>%
+  inner_join(arthro_df %>% select(StationCode) %>% unique()) %>%
+  group_by(ReachStress) %>% tally()
+
+
+ggplot(data=phabstress_df, aes(x=PHABMet_value))+
+  geom_histogram(aes(fill=PHABMet))
+
+
+
+
+library(corrplot)
+
   
-stress_df %>%
-  inner_join()
-             
-             #%>%
-             # mutate(SampleDate=lubridate::mdy(SampleDate))
-             ) %>%
-  filter(CollectionMethodCode=="TerInvt_T_DS") %>%
-  # ggplot(aes(x=PHABMet_value, y=BugMet_value))+
-  # # geom_point()+
-  # geom_smooth()+
-  # facet_grid(BugMet~PHABMet, scales="free")+
-  # theme_bw()+
-  # theme(strip.text.y=element_text(angle=0))+
-  # scale_x_sqrt()
   
   ggplot(aes(x=PHABMet_value, y=BugMet_value))+
   # geom_point()+
